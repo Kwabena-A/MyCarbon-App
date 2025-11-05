f1cf8da0d16ab09bc0f0ce9f33aebc5ebf8eeff4
kb <kwabena14817@gmail.com>
2025-11-05 15:56:19 +0400

    Added more questions

--- lib/data/values.dart
+++ lib/data/values.dart
@@ -23,26 +23,54 @@ ValueNotifier<String> singleSelected = ValueNotifier("");
 ValueNotifier<List<String>> multiSelected = ValueNotifier([]);
 
 final List<Question> questionList = [
-  Question("Hows the whether?", UserInputOptions.SINGLECHOICE, [
-    "Good",
-    "Mid Fr",
-    "Bad",
+  Question("Whats your gender?", UserInputOptions.SINGLECHOICE, [
+    "Male",
+    "Female",
   ]),
-  Question("What car do u drive", UserInputOptions.SINGLECHOICE, [
-    "Toyota",
-    "Prius",
-    "Lenoir Car",
+  Question(
+    "How would you categorize your body type?",
+    UserInputOptions.SINGLECHOICE,
+    ['overweight', 'obese', 'underweight', 'normal'],
+  ),
+  Question("What does your diet look like?", UserInputOptions.MULTICHOICE, [
+    'omnivore',
+    'vegetarian',
+    'vegan',
+    'pescatarian',
   ]),
-  Question("What do u cook with?", UserInputOptions.MULTICHOICE, [
-    "Stove",
-    "Pan",
-    "Grill",
-    "Yk, the usual",
+  Question("How often do you shower per day?", UserInputOptions.SINGLECHOICE, [
+    "Once",
+    "Twice",
+    "Three Times",
+    "Shower?",
   ]),
-  Question("What Powers Your Home? ", UserInputOptions.MULTICHOICE, [
-    "Oil",
-    "Gas",
-    "Solar/Renwable",
-    "ChatGPT",
+  Question(
+    "Whats your primary heating source?",
+    UserInputOptions.SINGLECHOICE,
+    ['wood', 'coal', 'electricity', 'natural gas'],
+  ),
+  Question(
+    "Whats your preferred mode of transport",
+    UserInputOptions.SINGLECHOICE,
+    ['public', 'walk/bicycle', 'private'],
+  ),
+  Question("Whats your vehicle type", UserInputOptions.SINGLECHOICE, [
+    'none',
+    'petrol',
+    'diesel',
+    'hybrid',
+    'lpg',
+    'electric',
+  ]),
+  Question("How socially active are you?", UserInputOptions.SINGLECHOICE, [
+    'often',
+    'never',
+    'sometimes',
+  ]),
+
+  Question("Whats your monthly grocery bill?", UserInputOptions.MULTICHOICE, [
+    'add number wheel',
+    'quickly',
+    'very fast',
   ]),
 ];
